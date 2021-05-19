dbe73e2b61543f27f7f904bba0d4d70701e53870
Ayush PS <ayushpratap16@gmail.com>
2021-05-20 00:28:30 +0530

    Hover desc

--- src/components/events.vue
+++ src/components/events.vue
@@ -1,41 +1,48 @@
 <template>
-  <agile
-    :rtl="false"
-    :dots="true"
-    :infinite="true"
-    :autoplay="true"
-    :autoplay-speed="5000"
-    :pauseOnHover="true"
-    :centerMode="true"
-  >
-    <div class="slide">
-      <a href="https://github.com/Catalyst-IN/Awesome-10-20-Frontend" target="_blank"
-        ><img src="../assets/e1020.png"
-      /></a>
-    </div>
-    <div class="slide">
-      <a
-        href="https://github.com/Catalyst-IN"
-        target="_blank"
-        ><img src="../assets/event2.png"
-      /></a>
-    </div>
-    <!--    <div class="slide">-->
-    <!--      <a href="https://github.com/Catalyst-IN" target="_blank"-->
-    <!--        ><img src="../assets/event4.png"-->
-    <!--      /></a>-->
-    <!--    </div>-->
-    <template slot="prevButton"><i class="fas fa-chevron-left"></i></template>
-    <template slot="nextButton"><i class="fas fa-chevron-right"></i></template>
-  </agile>
+    <agile
+        :rtl="false"
+        :dots="true"
+        :infinite="true"
+        :autoplay="true"
+        :autoplay-speed="5000"
+        :pauseOnHover="true"
+        :centerMode="true"
+    >
+        <div class="slide">
+            <a
+                href="https://github.com/Catalyst-IN/Awesome-10-20-Frontend"
+                target="_blank"
+                ><img src="../assets/e1020.png" />
+                <div class="slide_des"><p>Description goes here</p></div></a
+            >
+        </div>
+
+        <div class="slide">
+            <a href="https://github.com/Catalyst-IN" target="_blank"
+                ><img src="../assets/event2.png" />
+                <div class="slide_des"><p>Description goes here</p></div></a
+            >
+        </div>
+        <!--    <div class="slide">-->
+        <!--      <a href="https://github.com/Catalyst-IN" target="_blank"-->
+        <!--        ><img src="../assets/event4.png"-->
+        <!--      /></a>-->
+        <!--    </div>-->
+        <template slot="prevButton"
+            ><i class="fas fa-chevron-left"></i
+        ></template>
+        <template slot="nextButton"
+            ><i class="fas fa-chevron-right"></i
+        ></template>
+    </agile>
 </template>
 
 <script>
-import { VueAgile } from "vue-agile";
+import { VueAgile } from 'vue-agile';
 export default {
-  components: {
-    agile: VueAgile,
-  },
+    components: {
+        agile: VueAgile,
+    },
 };
 </script>
 
@@ -99,7 +106,27 @@ export default {
 	object-fit: cover
 	width: 100%
 .slide img
-  width: 100%
   height: 100%
+  width: 100%
   overflow: hidden
+.slide_des
+  height: 100%
+  width: 100%
+  background-color: black
+  position: fixed
+  top: 0
+  color: white
+  opacity: 0
+  transition: opacity 0.2s
+
+.slide_des p
+   position: absolute;
+   top: 50%;
+   left: 50%
+   transform: translate(-50%, -50%)
+
+
+
+.slide:hover .slide_des
+  opacity: 0.9
 </style>
